585b27f973d4b7023dae093673ef733f0e9fb3b5
Dennis Schoorl <dennisschoorl@hotmail.com>
2013-03-09 14:28:26 +0100

    My first server versie 2

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-var sys = require("sys"),
-my_http = require("http");
-my_http.createServer(function(request,response){
-	sys.puts("I got kicked");
-	response.writeHeader(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
-}).listen(8080);
-sys.puts("Server Running on 8080");
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
