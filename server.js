ef4b98addd83513a2ed0ad391e9a27ddfb8c8846
Dennis Schoorl <dennisschoorl@hotmail.com>
2013-03-09 22:09:56 +0100

    azureQueueTest

--- server.js
+++ server.js
@@ -1,6 +1,77 @@
+function azureQueueTest(request, response)
+{
+	var test= url.parse(request.url).path;
+	console.log(test);
+	var queuename= 'myqueue';
+
+	if(test='CREATE')
+	{
+		createQueueTest(queuename);
+		console.log("queue created...");
+	}
+
+	if(test= 'SEND') 
+	{
+		sendMessageTest(queuename);
+		console.log('enqueing message...');
+	} 
+
+	if(test= 'RECEIVE')
+	{
+		receiveMessageTest(queuename);
+	}
+	
+	response.end();
+}
+
+function createQueueTest(queuename)
+{
+     serviceBusService.createQueueIfNotExists(queuename,  function(error){
+        if(!error){
+           console.log('queue created...');
+		}
+	})
+}
+
+function sendMessageTest(queue)
+{
+	var message = {
+		body: 'Test message',
+		customProperties: {
+			testproperty: 'TestValue'
+		}}
+		
+		serviceBusService.sendQueueMessage(queue, message, function(error){
+        if(!error){
+           console.log('queue created...');
+		}})
+}
+
+serviceBusService.receiveQueueMessage('taskqueue', function(error, receivedMessage){
+    if(!error){
+        // Message received and deleted
+    }
+});
+serviceBusService.receiveQueueMessage(queueName, { isPeekLock: true }, function(error, lockedMessage){
+    if(!error){
+        // Message received and locked
+        serviceBusService.deleteMessage(lockedMessage, function (deleteError){
+            if(!deleteError){
+                // Message deleted
+            }
+        }
+    }
+});
+
+process.env.AZURE_SERVICEBUS_NAMESPACE= "geenid";
+process.env.AZURE_SERVICEBUS_ACCESS_KEY= "Bt0RlwPVgzug4Rizbf9UjMUel1vH5yOxOUx5R+jb9+I=";
+
+var url = require('url');
+var azure = require('azure');
+var serviceBusService = sb.createServiceBusService();
+
 var http = require('http')
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
-}).listen(port);
+var server = http.createServer(azureQueueTest);
+console.log('service listening at localhost:' + port + '...');
+server.listen(port); 
